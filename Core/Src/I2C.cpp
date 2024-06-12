b658b0ca773114cae372ddf5691bb2c86f171600
Markiv999 <vikram.srikanth.10@gmail.com>
2024-06-12 12:14:15 +0200

    Revert "ughh"
    This reverts commit ef2ada96b11c994bdb03b1f601ddde1ac0b94f03.

--- Core/Src/I2C.cpp
+++ Core/Src/I2C.cpp
@@ -9,7 +9,6 @@
 //Select I2C instance no from {1,2,3} (STM32NucleoL476 has 3 instances I2C1, I2C2, I2C3) and mode from {1=normal(100kbps), 2=fastmode(400kbps), 3=fastmodeplus(1Mbps)}
 I2C::I2C(int I2CInstanceNo, int mode,UART_HandleTypeDef huart2, uint32_t delay ) {
 <<<<<<< HEAD
-<<<<<<< HEAD
 	char* Error_Msg[25];
 	sprintf((char*)Error_Msg,
 									"%u, %u \r\n",
@@ -36,8 +35,6 @@ I2C::I2C(int I2CInstanceNo, int mode,UART_HandleTypeDef huart2, uint32_t delay )
 =======
 
 
-=======
->>>>>>> parent of ea59823 (Committing implementation)
 	char* Error_Msg[25];
 	sprintf((char*)Error_Msg,
 									"%u, %u \r\n",
